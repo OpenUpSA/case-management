1e41885911f88da8b2e34c80b998b4420139e33f
Paul Watson <github.com@paulmwatson.com>
2021-07-23 12:06:24 +0200

    Run migrate on deploy

--- bin/start.sh
+++ bin/start.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 gunicorn  --worker-class gevent case_management.wsgi:application --log-file - --bind 0.0.0.0:5000
