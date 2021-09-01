1f205737af3bf0e80631319a0ec1e6bdcda8a36f
Paul Watson <github.com@paulmwatson.com>
2021-09-01 09:54:23 +0200

    No nulls on user info

--- case_management/models.py
+++ case_management/models.py
@@ -12,7 +12,7 @@ from case_management.managers import UserManager
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255, null=False, blank=True, default="")
     membership_number = models.CharField(
         max_length=20, default='AA/B00/000', null=False, blank=False)
     email = models.EmailField(
@@ -20,7 +20,7 @@ class User(AbstractUser):
         max_length=255,
         unique=True,
     )
-    contact_number = PhoneNumberField(null=True, blank=True)
+    contact_number = PhoneNumberField(null=False, blank=True, default="")
     case_office = models.ForeignKey(
         'CaseOffice', related_name='users', on_delete=models.CASCADE, null=True, blank=True)
 
