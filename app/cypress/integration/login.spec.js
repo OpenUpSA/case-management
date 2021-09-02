c6837eb5a3ecc4bbdc628dcf6099679904ee4981
Paul Watson <github.com@paulmwatson.com>
2021-09-02 23:29:07 +0200

    Rename login spec test

--- app/cypress/integration/login.spec.js
+++ app/cypress/integration/login.spec.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 import "cypress-localstorage-commands";
 
-describe("home", () => {
-  it("displays home", () => {
+describe("login", () => {
+  it("displays login", () => {
     cy.setLocalStorage("user_id", "1");
     cy.visit("/login");
     cy.get("button").should("have.text", "Login");
