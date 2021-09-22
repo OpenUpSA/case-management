51352d6bb0ee57444e19411150b32ba7cebed5b6
Abdulhamid Usman <hazemidoo@gmail.com>
2021-09-22 16:55:02 +0100

    added cache limit

--- app/public/serviceworker.js
+++ app/public/serviceworker.js
@@ -1,48 +1,62 @@
-const staticCacheName = 'site-static-v1';
-const dynamicCacheName = 'site-dynamic-v1';
-const assets = [
-  '/',
-  'index.html'
-];
+const staticCacheName = "site-static-v1";
+const dynamicCacheName = "site-dynamic-v1";
+const assets = ["/", "index.html"];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 // install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed');
+self.addEventListener("install", (evt) => {
+  console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
+      console.log("caching shell assets");
       cache.addAll(assets);
     })
   );
 });
 
 // activate event
-self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
+self.addEventListener("activate", (evt) => {
+  console.log('service worker activated');
   evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
       );
     })
   );
 });
 
 // fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  if (!(evt.request.url.indexOf('http') === 0)) return; 
+self.addEventListener("fetch", (evt) => {
+  if (!(evt.request.url.indexOf("http") === 0)) return;
   evt.respondWith(
-    caches.match(evt.request, {ignoreVary: true}).then(cacheRes => {
-      //console.log(cacheRes)
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      });
-    }).catch(err => console.log(err))
+    caches
+      .match(evt.request, { ignoreVary: true })
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(dynamicCacheName, 50);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch((err) => console.log(err))
   );
 });
