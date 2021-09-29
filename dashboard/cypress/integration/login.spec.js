b63b73ece571da290e655f249471d264e0100ea7
Abdulhamid Usman <hazemidoo@gmail.com>
2021-09-29 13:13:07 +0100

    still on testing for dashboard

--- dashboard/cypress/integration/login.spec.js
+++ dashboard/cypress/integration/login.spec.js
@@ -1,11 +1,13 @@
+/// <reference types="cypress" />
+import "cypress-localstorage-commands";
+
 describe("Login", ()=>{
     it("logs in", ()=>{
         cy.visit("/login");
-        cy.get("input[name='email']").type("hazemidoo@gmail.com")
-        cy.get("input[name='password']").type("72fA0rRFwaTm1twHKUqP")
+        cy.get("input[name='email']").type("fake@fakemail.com")
+        cy.get("input[name='password']").type("fake1234")
         cy.get("button[type='submit']").click()
         cy.get("h1").should("have.text", "Reporting Dashboard");
-
     })
 })
 
