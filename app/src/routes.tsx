c51b6e45d795d019cfe270749272c6ada1e16637
Abdulhamid Usman <hazemidoo@gmail.com>
2021-10-28 18:13:46 +0100

    added google analytics to track page views

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -25,54 +25,49 @@ import NotFoundPage from "./pages/notFound";
 
 import Navigation from "./components/navigation";
 
-import LogsPage from "./pages/logs/list";
+// import LogsPage from "./pages/logs/list";
 
 function Routes() {
-
   return (
     <div>
       <Navigation />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
 
-        <Route exact path="/clients" component={ClientsPage} />
-        <Route exact path="/clients/new" component={ClientNewPage} />
-        <Route exact path="/clients/:id" component={ClientPage} />
-        <Route
-          exact
-          path="/clients/:id/edit"
-          component={ClientEditPage}
-        />
-        <Route
-          exact
-          path="/clients/:id/cases"
-          component={ClientLegalCasesPage}
-        />
+          <Route exact path="/clients" component={ClientsPage} />
+          <Route exact path="/clients/new" component={ClientNewPage} />
+          <Route exact path="/clients/:id" component={ClientPage} />
+          <Route exact path="/clients/:id/edit" component={ClientEditPage} />
+          <Route
+            exact
+            path="/clients/:id/cases"
+            component={ClientLegalCasesPage}
+          />
 
-        <Route exact path="/cases" component={LegalCasesPage} />
-        <Route exact path="/cases/:id" component={LegalCasePage} />
-        <Route
-          exact
-          path="/clients/:id/cases/new"
-          component={LegalCaseNewPage}
-        />
+          <Route exact path="/cases" component={LegalCasesPage} />
+          <Route exact path="/cases/:id" component={LegalCasePage} />
+          <Route
+            exact
+            path="/clients/:id/cases/new"
+            component={LegalCaseNewPage}
+          />
 
-        <Route exact path="/meetings" component={MeetingsPage} />
-        <Route exact path="/meetings/:id" component={MeetingPage} />
-        <Route exact path="/meetings/:id/edit" component={MeetingEditPage} />
-        <Route
-          exact
-          path="/cases/:id/meetings/new"
-          component={MeetingNewPage}
-        />
+          <Route exact path="/meetings" component={MeetingsPage} />
+          <Route exact path="/meetings/:id" component={MeetingPage} />
+          <Route exact path="/meetings/:id/edit" component={MeetingEditPage} />
+          <Route
+            exact
+            path="/cases/:id/meetings/new"
+            component={MeetingNewPage}
+          />
 
-        <Route exact path="/users/:id" component={UserPage} />
-        <Route exact path="/users/:id/edit" component={UserEditPage} />
+          <Route exact path="/users/:id" component={UserPage} />
+          <Route exact path="/users/:id/edit" component={UserEditPage} />
 
-        <Route exact path="/updates" component={LogsPage} />
+          {/* <Route exact path="/updates" component={LogsPage} /> */}
 
-        <Route component={NotFoundPage} />
+          <Route component={NotFoundPage} />
       </Switch>
     </div>
   );
