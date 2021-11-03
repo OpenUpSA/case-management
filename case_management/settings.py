da14b6ed1e15e00aa82157979acf59264e267c5a
Paul Watson <github.com@paulmwatson.com>
2021-11-03 09:07:09 +0200

    If no AWS S3 settings then do not set it, useful for dev

--- case_management/settings.py
+++ case_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import logging.config
+import os
 import environ
 
 ROOT_DIR = environ.Path(__file__) - 2
@@ -186,7 +187,8 @@ AUTH_USER_MODEL = 'case_management.User'
 PHONENUMBER_DB_FORMAT = 'NATIONAL'
 PHONENUMBER_DEFAULT_REGION = 'ZA'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME =  env('AWS_STORAGE_BUCKET_NAME')
+if os.getenv("AWS_ACCESS_KEY_ID"):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME =  env('AWS_STORAGE_BUCKET_NAME')
