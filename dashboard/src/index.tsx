8e7d0c3f11adbcd76fa869e990f59ba32567e447
Paul Watson <github.com@paulmwatson.com>
2022-01-05 13:34:53 +0200

    Added Sentry

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -9,6 +9,19 @@ import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
+import * as Sentry from  "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  
+  // tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
