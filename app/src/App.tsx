bd672c704e3ea6bab898c5e682c5bc3cc0cd911d
Abdulhamid Usman <hazemidoo@gmail.com>
2022-01-25 13:26:07 +0100

    used useContext for app wide state of case types and case offices

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,13 +2,19 @@ import { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 import "@fontsource/roboto";
+import { CaseOfficesProvider } from "./contexts/caseOfficesContext";
+import { CaseTypesProvider } from "./contexts/caseTypesContext";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Routes />
-      </Router>
+      <CaseTypesProvider>
+        <CaseOfficesProvider>
+          <Router>
+            <Routes />
+          </Router>
+        </CaseOfficesProvider>
+      </CaseTypesProvider>
     );
   }
 }
