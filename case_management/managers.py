2cc8ec79326f203a68da4265e3700cc2fdf689ed
rikusv <rikusv@gmail.com>
2022-03-30 21:16:49 +0200

    Assign superuser to Admin user group

--- case_management/managers.py
+++ case_management/managers.py
@@ -27,6 +27,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('permission_group', 'Admin')
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
