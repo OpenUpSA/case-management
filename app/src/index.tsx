0a7cf3cab8aa8a9f5fc7d4bcaa8a1403d88ebe3c
paulmwatson <github.com@paulmwatson.com>
2022-09-07 16:03:56 +0200

    Actually use Sentry

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -8,6 +8,17 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <Provider store={store}>
