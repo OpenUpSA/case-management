98ecc2a406c62eb69666abc6d26d987fb12e4e5b
paulmwatson <github.com@paulmwatson.com>
2023-03-08 11:49:36 +0200

    Do not fail if email settings not set

--- case_management/settings.py
+++ case_management/settings.py
@@ -245,13 +245,11 @@ if os.getenv("AWS_ACCESS_KEY_ID"):
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' 
-EMAIL_HOST_PASSWORD = env('SENDGRID_API_KEY')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = env('FROM_EMAIL')
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
+if os.getenv("EMAIL_HOST"):
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = env('EMAIL_PORT', default=587)
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = env('EMAIL_DEFAULT_FROM')
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
