3332a20b821b17c241a70dcb8e3da1f85600d4be
paulmwatson <github.com@paulmwatson.com>
2023-09-21 12:52:52 +0200

    Over Hotjar feedback

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -11,7 +11,7 @@ export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     "@global": {
       ".MuiDialog-root": {
-        zIndex: "2000!important",
+        zIndex: "2147483640!important",
       },
     },
     formError: {
