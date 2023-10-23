bb7cbfd9b4ed922695cb0cbf9f384676ce394b43
paulmwatson <github.com@paulmwatson.com>
2023-10-23 17:06:02 +0200

    Upgrade i18n and other bits

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -41,7 +41,7 @@ i18n
     lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-    missingKeyHandler: function (lng, ns, key, fallbackValue) {
+    missingKeyHandler: function (lng: any, ns: any, key: any, fallbackValue: any) {
       console.log('missing');
       console.log({ lng });
     },
