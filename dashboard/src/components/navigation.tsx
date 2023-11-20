fbf3581c165888666da9c58fba5e773eb5859c8a
paulmwatson <github.com@paulmwatson.com>
2023-11-20 16:24:44 +0200

    Empty alt

--- dashboard/src/components/navigation.tsx
+++ dashboard/src/components/navigation.tsx
@@ -92,6 +92,7 @@ const Component = () => {
                     history.push("/");
                     closeDrawer();
                   }}
+                  alt=""
                 />
               )}
             </Box>
